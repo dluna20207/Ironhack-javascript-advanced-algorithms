563b7db69ab25eaa6e628c9aa5b451391e2ec2cb
Luna Daniel <20207@columbushs.com>
2020-01-22 11:40:45 -0500

    Queue in progress

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,54 @@
-function QueueDataStructure () {
-}
+// Queue class
+class Queue {
+
+    
+    // Array is used to implement a Queue
+    constructor() {
+      this.items = [];
+    }
+  
+    // enqueue function
+    enqueue(element) {
+      // adding element to the queue
+      this.items.push(element);
+    }
+  
+    // dequeue function
+    dequeue() {
+      // removing element from the queue
+      // returns underflow when called
+      // on empty queue
+      if (this.isEmpty()) return "Queue Underflow";
+      return this.items.shift();
+    }
+  
+    // front function
+    front() {
+      // returns the Front element of
+      // the queue without removing it.
+      if (this.isEmpty()) return "No elements in Queue";
+      return this.items[0];
+    }
+  
+    // isEmpty function
+    isEmpty() {
+      // return true if the queue is empty.
+      return this.items.length == 0;
+    }
+    
+    // printQueue function
+    printQueue() {
+      let str = "";
+      for (let i = 0; i < this.items.length; i++) str += this.items[i] + " ";
+      return str;
+    }
+  }
+  
+  let queue = new Queue();
+  
+  
+  function QueueDataStructure() {
+  
+  
+  
+  }
